1397bd0519eaddb7d0097b48cb6dbcf807e1c61f
sushant12811 <steve.malistella@gmail.com>
2025-04-21 21:12:19 -0400

    added own color in tailwind.config

---
+++ nativewind-env.d.ts
@@ -0,0 +1,16 @@
+///<reference types ="nativewind/types" />
+
+/*
+-> This is TypeScript syntax that tells TypeScript compiler to include or load the types from the
+nativewind/types.
+
+-> nativewind is a library that brings Tailwind CSS-style utility classes to React Native.
+
+-> It's like using className="bg-red-500 p-4" in React Native components instead of StyleSheet.create().
+
+-> with the help of this reference, nativewind modifies React Native components (View, Text, etc.)
+ to accept className and understand it.
+
+ -> It prevents TypeScript errors when using NativeWind utilities.
+
+ */
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#030014',
+        secondary: '#151312',
+        light:{
+          100: '#D6C6FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB'
+        },
+        dark:{
+          100: '#221F3D',
+          200: '#0F023D'
+        },
+        accent: '#AB8BFF'
+      }
+    },
   },
   plugins: [],
 }
