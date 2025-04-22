ee0c8f3097550832dc0462e4ca77753c987b1e86
sushant12811 <steve.malistella@gmail.com>
2025-04-22 10:46:05 -0400

    added screens in rootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"; //
 import './globals.css'
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>            //Stack navigator- manages multiple screens inside it.
+    <Stack.Screen           //This is a single Screen name ("tabs")and options helps not to show header in that screen
+      name = "(tabs)"
+      options = {{headerShown : false}}
+    />
+
+    <Stack.Screen    //This is a single Screen name ("movie/[id]")and options helps not to show header in that screen
+    name = "movie/[id]"
+    options = {{headerShown : false}}
+    />
+
+  </Stack>;
 }
+
+/*
+We are setting up navigation for expo-router
+Expo router is a file-based router for react-native - Similar to Next.js for web
+It automatically turns files and folder into screens and routes.
+
+ */
